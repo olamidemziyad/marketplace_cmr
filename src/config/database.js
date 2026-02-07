1de711d1361649c1de125d4fd1c0ac1e9763b25a
OlamideZiyad <olamidezyad@gmail.com>
2026-02-07 10:50:57 +0100

    Make Sequelize MySQL config compatible with Railway

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,14 @@
 const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.MYSQLDATABASE || process.env.DB_NAME,
+  process.env.MYSQLUSER || process.env.DB_USER,
+  process.env.MYSQLPASSWORD || process.env.DB_PASSWORD,
   {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    host: process.env.MYSQLHOST || process.env.DB_HOST,
+    port: process.env.MYSQLPORT || process.env.DB_PORT,
     dialect: "mysql",
-    logging: false, // mettre true on  veux voir les requêtes
+    logging: false,
   }
 );
 
